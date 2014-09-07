90071d7a578eb46a9f5bb12339639ce4a296ee31
Takayuki Yorikane <yorikane_takayuki@cyberagent.co.jp>
2014-09-07 12:46:25 +0900

    Locationクラス作成

---
+++ app/src/main/java/com/hackathon/liveeye/dto/Location.java
@@ -0,0 +1,13 @@
+package com.hackathon.liveeye.dto;
+
+/**
+ * Created by yorikane_takayuki on 2014/09/07.
+ */
+public class Location {
+
+    public double latitude;
+    public double longitude;
+    public float speed;
+    public long time;
+    public double altitude;
+}
